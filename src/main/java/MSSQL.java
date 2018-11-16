964694ede1b359768e2e1bda017b37e729e58f1e
a.janars@gmail.com <Sekrets123>
2018-11-17 02:23:49 +0600

    MSSQL date1 and date2

--- src/main/java/MSSQL.java
+++ src/main/java/MSSQL.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class MSSQL {
 
@@ -15,10 +16,16 @@ public class MSSQL {
         String connectUrl = "jdbc:sqlserver://185.97.115.127\\RUSGUARD;database=RusGuardDB";
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         System.out.println("gvjhgjkghkj");
-        String date = new SimpleDateFormat("yyyyMMdd").format(Calendar.getInstance().getTime());
+        Calendar c = new GregorianCalendar();
+        String date2 = new SimpleDateFormat("yyyyMMdd").format(c.getTime());
+        System.out.println(c.getTime());
+        System.out.println(date2);
+        c.add(Calendar.DAY_OF_YEAR, -7);
+        String date1 = new SimpleDateFormat("yyyyMMdd").format(c.getTime());
+        System.out.println(date1);
         try (Connection conn = DriverManager.getConnection(connectUrl, userName, password); Statement stmt = conn.createStatement();) {
             String SQL = "SELECT [Employee].[SecondName], [Employee].[FirstName], [Employee].[LastName], [Log].[DateTime], [Log].[Message]  FROM [RusGuardDB].[dbo].[Employee]" +
-                    "JOIN [RusGuardDB].[dbo].[Log] ON [Log].[EmployeeID] = [Employee].[_id]  WHERE [Log].[DateTime] > '"+date+"' and [Employee].[PassportNumber] LIKE '%"+message+"%'";
+                    "JOIN [RusGuardDB].[dbo].[Log] ON [Log].[EmployeeID] = [Employee].[_id]  WHERE ([Log].[DateTime] > '"+date1+"' and [Log].[DateTime] < '"+date2+"') and [Employee].[PassportNumber] LIKE '%"+message+"%'";
             System.out.println(SQL);
             ResultSet rs = stmt.executeQuery(SQL);
             System.out.println(rs);
