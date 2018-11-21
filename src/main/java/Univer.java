b9f1cd13ea9814497b4078c5ab08dab08ba327c6
Zhanars <a.janars@gmail.com>
2018-11-21 21:40:04 +0600

    first commit

--- src/main/java/Univer.java
+++ src/main/java/Univer.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
 import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Univer {
     private static String countName = "";
@@ -86,13 +89,20 @@ public class Univer {
 
         System.out.println("gvjhgjkghkj");
         String SQL = "";
+        Calendar c = new GregorianCalendar();
+        String date2 = new SimpleDateFormat("yyyyMMdd").format(c.getTime());
+        System.out.println(c.getTime());
+        System.out.println(date2);
+        c.add(Calendar.DAY_OF_YEAR, -7);
+        String date1 = new SimpleDateFormat("yyyyMMdd").format(c.getTime());
+
         try (Connection conn = DriverManager.getConnection(connectUrl, userName, password); Statement stmt = conn.createStatement();) {
                 SQL = "SELECT [univer_subject].[subject_name_ru] ,[univer_attendance].[att_date], [univer_attendance].[ball] FROM [atu_univer].[dbo].[univer_students]" +
                         "JOIN  [atu_univer].[dbo].[univer_attendance] ON [univer_attendance].[student_id] = [univer_students].[students_id]" +
                         "JOIN  [atu_univer].[dbo].[univer_group] ON [univer_group].[group_id] = [univer_attendance].[group_id]" +
                         "JOIN  [atu_univer].[dbo].[univer_educ_plan_pos] ON [univer_educ_plan_pos].[educ_plan_pos_id] = [univer_group].[educ_plan_pos_id]" +
                         "JOIN  [atu_univer].[dbo].[univer_subject] ON [univer_subject].[subject_id] = [univer_educ_plan_pos].[subject_id]" +
-                        "WHERE [univer_students].[students_identify_code] LIKE '%" + IIN + "%' and [univer_students].[student_edu_status_id] = 1  ";
+                        "WHERE [univer_students].[students_identify_code] LIKE '%" + IIN + "%' and [univer_students].[student_edu_status_id] = 1 and [univer_attendance].[att_date] > '"+date1+"' ";
 
             ResultSet rs1 = stmt.executeQuery(SQL);
             int columns1 = rs1.getMetaData().getColumnCount();
