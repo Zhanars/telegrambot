dd80a86ce190820bbed4dee8b584c80444e443fd
a.janars@gmail.com <Sekrets123>
2018-12-15 16:57:45 +0600

    Default Changelist

--- src/main/java/umkd.java
+++ src/main/java/umkd.java
@@ -1,56 +1,40 @@
 import jcifs.smb.NtlmPasswordAuthentication;
-import jcifs.smb.SmbException;
-import jcifs.smb.SmbFile;
-import jcifs.smb.SmbFileInputStream;
+import okhttp3.*;
+
 import java.io.*;
-import java.net.MalformedURLException;
-import java.net.UnknownHostException;
 
 public class umkd {
     public static void getUMKD(Long ChatId, String TeacherId, String fileName) {
         InputStream in = null;
         OutputStream out = null;
-        String url = "smb://185.97.115.134/umkd/" + TeacherId + "/"+fileName;
+        String url = "smb://185.97.115.134/umkd/" + TeacherId + "/" + fileName;
         NtlmPasswordAuthentication auth = new NtlmPasswordAuthentication(null, "Администратор", "Univer2018#");
-        SmbFile dir = null;
+        String HTTP = "https://api.telegram.org/bot745779362:AAEFky83gaEOP4aB8RjUHq4_BcW7hCYSB68/sendDocument?chat_id=" + ChatId;
+        String fileSource = url.substring(4);
+        OkHttpClient client = new OkHttpClient();
+        File sourceFile = new File(fileSource);
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("document", sourceFile.getName(), RequestBody.create(MediaType.parse("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"), sourceFile))
+                .build();
+
+        Request request = new Request.Builder()
+                .url(HTTP)
+                .post(requestBody)
+                .build();
+
+        Response response = null;
         try {
-            dir = new SmbFile(url, auth);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        try {
-                String DocName = fileName;
-                File child = new File(DocName);
-                dir.connect();
-                in = new BufferedInputStream(new SmbFileInputStream(dir));
-                out = new BufferedOutputStream(new FileOutputStream(child));
-                byte[] buffer = new byte[4096];
-                int len = 0; //Read length
-                while ((len = in.read(buffer, 0, buffer.length)) != -1) {
-                    out.write(buffer, 0, len);
-                }
-                out.flush();
-                try {
-                    if (out != null) {
-                        out.close();
-                    }
-                    if (in != null) {
-                        in.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Bot.sendFile(ChatId, DocName);
-        } catch (SmbException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
+            response = client.newCall(request).execute();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        try {
+            String responseString = response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+
         }
     }
 }
