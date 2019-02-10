27a8b125aecdef06022048354aad5ca7b885b865
a.janars@gmail.com <Sekrets123>
2019-02-10 21:16:23 +0600

    last 23.01.2019

--- src/main/java/newsAtu.java
+++ src/main/java/newsAtu.java
@@ -1,25 +1,33 @@
-import firstmenu.Configuration;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 
-import java.sql.*;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class newsAtu {
     private static String result = "";
-    private static String userName = Configuration.getMySQLUsername();
-    private static String password = Configuration.getPass();
-    private static String connectUrl = Configuration.getMySQLHost();
     public static void inserChatId(Long ChatId){
-
-        try  ( Connection conn = DriverManager.getConnection(connectUrl, userName, password); Statement stmt = conn.createStatement();) {
-
-            String SelectSql = "SELECT * FROM ChatIDs  WHERE ChatId = '" + ChatId + "'";
-            ResultSet rs = stmt.executeQuery(SelectSql);
-            if (!rs.next()) {
-                String InsertSql = "INSERT INTO ChatIDs (ChatId) VALUES ('" + ChatId + "') ";
-                stmt.executeUpdate(InsertSql);
-            }
-
-        } catch (SQLException e) {
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost request = new HttpPost("http://telegram.atu.kz/api/createChat.php");
+        StringEntity params = null;
+        try {
+            params = new StringEntity("{\"name\":\"" + ChatId + "\"} ");
+            request.addHeader("content-type", "application/x-www-form-urlencoded");
+            request.setEntity(params);
+            HttpResponse response = httpClient.execute(request);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
